b2f83a4877bc22af3f968d9f5d3d6d8f171957bb
GK <gk@rb2.nl>
2022-11-28 23:21:16 +0100

    feat: Support extend renewal date api

---
+++ lib/app_store/api/extend_renewal_date.ex
@@ -0,0 +1,25 @@
+defmodule AppStore.API.ExtendRenewalDate do
+  @moduledoc """
+  The module for extending renewal date
+  """
+
+  alias AppStore.API.{Config, Error, Response, HTTP}
+
+  @type original_transaction_id :: String.t()
+
+  @path_prefix "/inApps/v1/subscriptions/extend"
+
+  @doc """
+  Extend the renewal date of a customer’s active subscription using the original transaction identifier.
+
+  Official documentation: https://developer.apple.com/documentation/appstoreserverapi/extend_a_subscription_renewal_date
+  """
+  @spec extend(Config.t(), String.t(), original_transaction_id, map) ::
+          {:error, Error.t()} | {:ok, Response.t()}
+  def extend(%Config{} = api_config, token, original_transaction_id, body)
+      when is_map(body) do
+    path = "#{@path_prefix}/#{original_transaction_id}"
+
+    HTTP.put(api_config, token, path, body)
+  end
+end
---
+++ test/app_store/api/extend_renewal_date_test.exs
@@ -0,0 +1,26 @@
+defmodule AppStore.API.ExtendRenewalDateTest do
+  use AppStore.TestCase, async: false
+
+  alias AppStore.API.ExtendRenewalDate
+
+  describe "extend/3" do
+    test "Get a history of transactions", %{bypass: bypass, app_store: app_store} do
+      Bypass.expect_once(bypass, "PUT", "/inApps/v1/subscriptions/extend/the-transaction-id", fn conn ->
+        conn
+        |> Plug.Conn.put_resp_header("server", "daiquiri/3.0.0")
+        |> Plug.Conn.resp(401, "Unauthenticated\n\nRequest ID: PXYVB35MOBBC5TL6UOXY6DGJGY.0.0\n")
+      end)
+
+      {:ok, %AppStore.API.Response{body: body, status: status}} =
+        ExtendRenewalDate.extend(
+          app_store.api_config,
+          "token",
+          "the-transaction-id",
+          %{}
+        )
+
+      assert status === 401
+      assert body === "Unauthenticated\n\nRequest ID: PXYVB35MOBBC5TL6UOXY6DGJGY.0.0\n"
+    end
+  end
+end
